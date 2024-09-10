2b763905ac4d4b977d6d777eb42b24e831ead697
surendra2chandu <surendra2chandu@gmail.com>
2024-09-10 13:20:19 +0530

    completed RobertaTextRouter.py and RobertaForTextClient.py

--- src/routers/RobertaTextRouter.py
+++ src/routers/RobertaTextRouter.py
@@ -1,20 +1,26 @@
 # Importing necessary classes
 from fastapi import APIRouter
 from src.api import RobertaForText
+from pydantic import BaseModel
 
 # Initialize the router
 router = APIRouter(
-    prefix="/question-answering-roberta",
+    prefix="/question_answering_roberta",
     tags=["question-answering"],
 )
 
 
+# Define the request body model
+class Questions(BaseModel):
+    questions: list[str]    # List of questions
+
+
 # Define the route
-@router.post("/text/")
-async def roberta_question_answering_from_text(questions: list[str], context: str) -> dict:
+@router.post("_text/")
+async def roberta_question_answering_from_text(questions: Questions, context: str) -> dict:
 
     # Call the question_answering_from_text function from RobertaForText.py
-    res = RobertaForText.question_answering_from_text(questions, context)
+    res = RobertaForText.question_answering_from_text(questions.questions, context)
 
     # Return the response
     return res

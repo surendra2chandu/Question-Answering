8625d1d1fc3c6bb0ed24c9fccf83f0b147c139df
surendra2chandu <surendra2chandu@gmail.com>
2024-09-10 17:27:11 +0530

    created 2 services

--- client/src/main.py
+++ client/src/main.py
@@ -3,6 +3,7 @@ from client.src.routers.RobertafRouterForPDF import router as roberta_pdf_router
 from client.src.routers.RobertaRouterForText import router as roberta_text_router
 
 
+# Initialize the FastAPI app
 app = FastAPI()
 
 app.include_router(roberta_pdf_router)

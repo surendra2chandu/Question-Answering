6ae00fc267ef2ff40ac6153619b686e1303a4a3f
Nemalipuri Venkata gopichand <surendra2chandu@gmail.com>
2024-09-23 18:04:50 +0530

    update

---
+++ smp3.py
@@ -0,0 +1,33 @@
+from ctransformers import AutoModelForCausalLM
+from llm.src.conf.Configurations import llama2_model_path, logger
+
+# Log loading message
+logger.info("Loading Llama2 model")
+
+# Load the Llama2 model
+llm = AutoModelForCausalLM.from_pretrained(llama2_model_path, model_type='llama')
+
+# Define the context and query
+context = "The Eiffel Tower is located in Paris, France."
+queries = ["What is the capital of france", "What is the capital of India?"]
+
+
+# Log generation message
+# Prepare the prompt for the model with clearer instructions
+# prompt = f"""Answer based on context. If the answer cannot be found in the context, respond with 'ER001: I cannot find answer in context. :\n\n{context}\n\n{query}"""
+prompt = ( f"""Use the following pieces of information to answer the user's queries.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Context: {context}
+Question: {queries}
+Only return the helpful answers below and nothing else.
+Helpful answer:
+"""
+)
+
+print(prompt)
+# Log generation message
+logger.info("Generating response from Llama2 model")
+
+# Generate and print the model's response
+response = llm(prompt=prompt, temperature=0.1, max_new_tokens=1024)
+print("response : ", response)
